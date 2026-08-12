417f9c9b60c171307f42d55ce09723cd7d6cea16
meetcc06 <meetcc06@gmail.com>
2026-08-12 23:54:58 +0530

    Revert "added 4 services"
    This reverts commit 07c53272a78314b04b9420708c106297537b00d4.

--- backend/notification-service/src/app.ts
+++
@@ -1,5 +0,0 @@
-import express from "express"
-import  { globalErrorHandler } from "@irctc/shared/src/middlewares/globalErrorHandler"
-
-export const app = express();
-app.use(globalErrorHandler);
--- backend/user-service/src/utils/wrapper.ts
+++
@@ -1,7 +0,0 @@
-    import { NextFunction, Request, RequestHandler, Response } from "express";
-
-    export const asyncWrapper = (requestHandler: RequestHandler) => {
-        return (req: Request, res: Response, next: NextFunction) => {
-            Promise.resolve(requestHandler(req, res, next)).catch(next)
-        };
-    };
